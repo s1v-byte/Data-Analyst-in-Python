353f16cc37691a0aedbc9ef8a3e096644f48b275
Dave <surigaojohndave@gmail.com>
2026-04-16 18:48:08 +0800

    Intro to Python List and Functions

---
+++ IntroToPython.py
@@ -0,0 +1,25 @@
+#Python List
+list1 = [[1.73, 1.68,2],[ 1.71, 1.89,3]]
+#print(list1[1][:])
+
+#Functions and Packages
+list1 = [[1.73, 1.98,2],[ 1.71, 1.89,3]]
+list2 =  ["height", 6.6, "weight", 50, "age", 23]
+list3 = ["max", "nim"]
+
+    #max
+#print(max(list2))
+
+    #replace in list
+replace = list3[1] = "min"
+print(list3)
+
+    #index method
+print(list2.index("weight"))
+
+    #append
+append1 = list3.append("median")
+print(list3)
+
+ 
+
